7ada2f189259bf838da3e8bace82720513583b33
Rayan <154925122+Rayanlupo@users.noreply.github.com>
2024-08-27 09:30:49 +0000

    still a lot of work to do

--- sketch.py
+++ sketch.py
@@ -1,8 +1,36 @@
-text = input("Time to CET")
+timezones = {
+    "GMT" : "Europe/London",
+    "CET" : "Europe/Paris",
+    "EET" : "Europe/Athens",
+    "MSK" : "Europe/Moscow",
+    "EST" : "America/New_York",
+    "CST" : "America/Chicago",
+    "MST" : "America/Denver",
+    "PST" : "America/Los_Angeles",
+    "IST" : "Asia/Kolkata",
+    "CST" : "Asia/Shanghai",
+    "JST" : "Asia/Tokyo",
+    "AEST" : "Australia/Sydney",
+    "NZST" : "Pacific/Auckland",
+    "SAST" : "Africa/Johannesburg",
+    "WAT" : "Africa/Lagos",
+    "AST" : "Asia/Riyadh",
+    "ART" : "America/Argentina/Buenos_Aires",
+    "BRT" : "America/Sao_Paulo",
+    "CLT" : "Santiago", 
+} 
+import pytz
+from datetime import datetime
+text = input("Time to CET\n")
 text = text.split(" ")
 time = text[0]
 timezone = text[1]
 timezone = timezone.upper();
-time_cet = time.astimezone(timezone('Europe/Rome'))
-response = time_cet
-print(response)
+if timezone in timezones:
+    time = datetime.strptime(time, '%H:%M')
+    timezone = timezones[timezone]
+    time_cet = time.astimezone(pytz.timezone('Europe/Paris'))
+    response = time_cet.strftime('%H:%M %Z')
+    print(response)
+else:
+    print("Invalid Timezone")
